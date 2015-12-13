58ca30008e63d82c8ff7ec02fe882cdf897166b3
Patrick Beeson <patrickbeeson@gmail.com>
2015-12-13 09:35:16 -0800

    Refactor static assets
    Wherein I compress and collect all static assets to new location and
    add back whitenoise for static serving.

--- durangoproductive/durangoproductive/settings/base.py
+++ durangoproductive/durangoproductive/settings/base.py
@@ -11,6 +11,9 @@ DJANGO_ROOT = dirname(dirname(abspath(__file__)))
 # Absolute filesystem path to the top-level project folder:
 SITE_ROOT = dirname(DJANGO_ROOT)
 
+# Absolute filesystem path to the folder containing the project
+REPO_ROOT = dirname(SITE_ROOT)
+
 # Site name:
 SITE_NAME = basename(DJANGO_ROOT)
 
@@ -94,7 +97,7 @@ MEDIA_URL = '/media/'
 
 #  STATIC FILE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
+STATIC_ROOT = normpath(join(REPO_ROOT, 'staticfiles'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
@@ -216,8 +219,10 @@ COMPRESS_PRECOMPILERS = (
     ('text/jsx', 'cat {infile} | jsx > {outfile}'),
 )
 COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
 COMPRESS_ROOT = normpath(join(SITE_ROOT, 'static'))
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #  LOGGING CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
