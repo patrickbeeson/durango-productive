b0dc5316e3ce2cacac0d6bc7dddc09605896d9ab
Patrick Beeson <patrickbeeson@gmail.com>
2016-02-15 08:30:29 -0800

    Fix issue where csrf was not loaded for contact form.

--- durangoproductive/static/js/csrf.js
+++ durangoproductive/static/js/csrf.js
@@ -20,7 +20,7 @@ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
-$.ajaxSetup({
+jQuery.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
