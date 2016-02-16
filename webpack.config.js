459de97fbf03d79e0b7dc2028f95548802203326
Patrick Beeson <patrickbeeson@gmail.com>
2016-02-15 21:13:17 -0800

    Add ability to use sass in webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,14 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
     ],
   },
 
